8d26bd5a9fbe3f39fd641e7bca54fecfa0a7fcd2
xiaopengyaa <857763476@qq.com>
2021-07-11 15:10:46 +0800

    feat: 依赖安装、配置处理

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {
+    'no-unused-vars': 'error',
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'always',
+          normal: 'always',
+          component: 'always',
+        },
+      },
+    ],
+  },
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import legacy from '@vitejs/plugin-legacy'
+import path from 'path'
+const resolve = (dir: string) => path.join(__dirname, dir)
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': resolve('./src'),
+    },
+  },
+  server: {
+    fs: {
+      allow: ['./'],
+    },
+  },
 })
